66616ee777fc3a401f41678079eb11154847e3b7
Okan Guner <okngnr@hotmail.com>
2018-08-08 17:38:07 +0300

    partial load düzenlendi

--- src/lib/VgtPartialLoading.js
+++ src/lib/VgtPartialLoading.js
@@ -11,11 +11,11 @@ class VgtPartialLoading {
         var top = ($(container).innerHeight() - 32) / 2;
         var left = ($(container).innerWidth() - 32) / 2;
         
-        $(container).append('<div class="vgt-loading-overlay" style="position:absolute;></div>');
+        $(container).append('<div class="vgt-loading-overlay" style="position:absolute;"></div>');
 
         $(container).append('\
-                <div class="vgt-load-progress" style="position:absolute;">\
-                    <div class="progress">\
+                <div class="vgt-load-progress" style="position:absolute; top:0;">\
+                    <div class="progress" style="margin:0;">\
                     <div class="indeterminate"></div>\
                     </div>\
                 </div>\
